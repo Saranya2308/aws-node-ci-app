c6753084fc4dc587e2a2f3be08e420fc6bb42c27
Saranya2308 <saranyachandu23@gmail.com>
2025-06-19 13:50:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
-
 app.get('/', (req, res) => {
-  res.send('🚀 Hello from AWS Beanstalk + CodePipeline CI/CD!');
+  res.send('Hello from AWS CI/CD pipeline!');
 });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`App running on port ${port}`);
 });
